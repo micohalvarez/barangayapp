19c35d0066a9e76b62e90005ea2e71a7bb7d39b7
Vincent Matheo <vincent_matheo246052@yahoo.com>
2021-04-03 17:01:46 +0800

    add

--- app/src/main/java/com/release/barangayapp/MainActivity.java
+++ app/src/main/java/com/release/barangayapp/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                System.out.println("tester");
             }
         });
     }
@@ -50,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-        System.out.println("tester");
+
 
         return super.onOptionsItemSelected(item);
 
