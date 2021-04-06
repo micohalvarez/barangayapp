b49d5964a357b159c3524575c4a4876d735fdf54
channnnssss <chanlcoffin@gmail.com>
2021-04-06 23:33:33 +0800

    User Main Menu Demo

--- app/src/main/java/com/release/barangayapp/UserMainMenu.java
+++ app/src/main/java/com/release/barangayapp/UserMainMenu.java
@@ -1,14 +1,14 @@
 package com.release.barangayapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-
-public class BarangayLogin extends AppCompatActivity {
+public class UserMainMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_barangay_login);
+        setContentView(R.layout.activity_user_main_menu);
     }
 }
