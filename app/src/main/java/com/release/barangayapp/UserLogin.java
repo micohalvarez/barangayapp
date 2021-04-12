001cb08d904f59d94da5fab4024bbc2b0289b9ac
channnnssss <chanlcoffin@gmail.com>
2021-04-13 00:38:18 +0800

    NavigationView-notworking
    The navigation view of user main menu still not working

--- app/src/main/java/com/release/barangayapp/UserLogin.java
+++ app/src/main/java/com/release/barangayapp/UserLogin.java
@@ -2,14 +2,30 @@ package com.release.barangayapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class UserLogin extends AppCompatActivity {
+
+    private Button Login_user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+
+
+        Login_user= findViewById(R.id.User_btn_signin);
+        Login_user.setOnClickListener(v -> LoginUser());
     }
+
+    public void LoginUser()
+    {
+        Intent intent=new Intent(this, UserMainMenu.class);
+        startActivity(intent);
+    }
+    @Override
+    public void onBackPressed() { }
 
 }
--- app/src/main/java/com/release/barangayapp/UserMainMenu.java
+++ app/src/main/java/com/release/barangayapp/UserMainMenu.java
@@ -1,16 +1,20 @@
 package com.release.barangayapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class UserMainMenu extends AppCompatActivity {
+public class UserMainMenu extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
@@ -24,8 +28,9 @@ public class UserMainMenu extends AppCompatActivity {
 
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
-        toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.tool_bar);
 
+        navigationView.bringToFront();
         setSupportActionBar(toolbar);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
@@ -33,9 +38,27 @@ public class UserMainMenu extends AppCompatActivity {
         toggle.syncState();
 
 
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_view);
+
+
+
+
     }
 
+    @Override
+    public void onBackPressed() {
 
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
 
-
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
+    }
 }
