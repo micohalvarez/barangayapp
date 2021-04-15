f8ff5920323da6fadb8ef9f77051ed0b9e91d865
channnnssss <chanlcoffin@gmail.com>
2021-04-15 16:12:38 +0800

    GridLayout

--- app/src/main/java/com/release/barangayapp/Emergency.java
+++ app/src/main/java/com/release/barangayapp/Emergency.java
@@ -1,14 +1,50 @@
 package com.release.barangayapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.GridLayout;
 
 public class Emergency extends AppCompatActivity {
+    GridLayout mainGrid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency);
+        setSingleEvent(mainGrid);
     }
+
+    private void setSingleEvent(GridLayout mainGrid) {
+        for(int i=0; i<mainGrid.getChildCount();i++)
+        {
+            CardView cardview = (CardView)mainGrid.getChildAt(i);
+            final int finalI = i;
+            cardview.setOnClickListener(v -> {
+
+                if(finalI == 0)
+                {
+
+                }
+                else if (finalI == 1)
+                {
+
+                }
+                else if (finalI == 2)
+                {
+
+
+                }
+                else if (finalI == 3)
+                {
+
+
+                }
+
+            });
+        }
+    }
+
 }
--- app/src/main/java/com/release/barangayapp/UserMainMenu.java
+++ app/src/main/java/com/release/barangayapp/UserMainMenu.java
@@ -3,20 +3,26 @@ package com.release.barangayapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.GridLayout;
 
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class UserMainMenu extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout drawerLayout;
+    GridLayout mainGrid;
     NavigationView navigationView;
     Toolbar toolbar;
 
@@ -24,6 +30,8 @@ public class UserMainMenu extends AppCompatActivity implements NavigationView.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_main_menu);
+        mainGrid = findViewById(R.id.mainGrid);
+        setSingleEvent(mainGrid);
 
 
         /*drawerLayout = findViewById(R.id.drawer_layout);
@@ -44,6 +52,38 @@ public class UserMainMenu extends AppCompatActivity implements NavigationView.On
 
 
 
+    }
+
+
+    private void setSingleEvent(GridLayout mainGrid) {
+        for(int i=0; i<mainGrid.getChildCount();i++)
+        {
+            CardView cardview = (CardView)mainGrid.getChildAt(i);
+            final int finalI = i;
+            cardview.setOnClickListener(v -> {
+
+                if(finalI == 0)
+                {
+
+                }
+                else if (finalI == 1)
+                {
+                    Intent intent = new Intent(UserMainMenu.this, Emergency.class);
+                    startActivity(intent);
+                }
+                else if (finalI == 2)
+                {
+
+
+                }
+                else if (finalI == 3)
+                {
+
+
+                }
+
+            });
+        }
     }
 
     @Override
