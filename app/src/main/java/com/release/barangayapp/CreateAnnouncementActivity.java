a3f9b2d1c028b63f90fa0360cf8ec2ad34924451
Vincent Matheo <vincent_matheo246052@yahoo.com>
2021-04-27 08:23:40 +0800

    pus

--- app/src/main/java/com/release/barangayapp/CreateAnnouncementActivity.java
+++ app/src/main/java/com/release/barangayapp/CreateAnnouncementActivity.java
@@ -2,18 +2,25 @@ package com.release.barangayapp;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class CreateAnnouncementActivity extends AppCompatActivity {
+
+    private ArrayList<String> icons;
 
     ImageView iconType, sendIcon;
     EditText subjectSection,announcementDetail;
-    TextView editIcon;
+    Spinner editIcon;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,10 +28,32 @@ public class CreateAnnouncementActivity extends AppCompatActivity {
         setContentView(R.layout.activity_barangay_createannouncement);
 
         subjectSection = findViewById(R.id.CAnnounceSubject);
-        editIcon = findViewById(R.id.CAnnounceEditIcon);
+        editIcon = (Spinner) findViewById(R.id.spinnerEditIcon);
         iconType = findViewById(R.id.CAnnounceIconType);
         sendIcon = findViewById(R.id.CAnnounceSendIcon);
         announcementDetail = findViewById(R.id.CAnnounceDetail);
+
+        this.initializeDropdown();
+        ArrayAdapter<String> iconAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, this.icons);
+        iconAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
+        editIcon.setAdapter(iconAdapter);
+
+        editIcon.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position)
+                {
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         sendIcon.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +63,17 @@ public class CreateAnnouncementActivity extends AppCompatActivity {
         });
 
 
+
+    }
+
+    private void initializeDropdown(){
+        this.icons = new ArrayList<>();
+        //icon labels
+        this.icons.add("Edit Icon");
+        this.icons.add("Fire");
+        this.icons.add("Weather");
+        this.icons.add("Covid");
+        this.icons.add("Crime");
 
     }
 }
