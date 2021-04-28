1768c0ff19118860d897afdd813f77371d5aff70
channnnssss <chanlcoffin@gmail.com>
2021-04-28 19:30:55 +0800

    EditNotificationAndAnnouncement Module

---
+++ app/src/main/java/com/release/barangayapp/view/NotificationsActivity.java
@@ -0,0 +1,38 @@
+package com.release.barangayapp.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.release.barangayapp.R;
+
+public class NotificationsActivity extends AppCompatActivity {
+
+    FloatingActionButton Addannouncement;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifications);
+
+        Addannouncement = findViewById(R.id.AddAnnouncement);
+
+        Addannouncement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Go to Add Announcement
+
+
+                //sample only
+                Intent emergency=new Intent(NotificationsActivity.this, Emergency.class);
+                startActivity(emergency);
+            }
+        });
+
+
+    }
+}
