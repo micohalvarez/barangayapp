ff560bc770b76c353e39813f72d52d5de5af790c
Maryems27 <81849739+Maryems27@users.noreply.github.com>
2021-05-19 00:10:44 +0800

    aesthetics_update

--- app/src/main/java/com/release/barangayapp/FirstFragment.java
+++ app/src/main/java/com/release/barangayapp/FirstFragment.java
@@ -1,15 +1,15 @@
 package com.release.barangayapp;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
 public class FirstFragment extends Fragment {
+
 
 
 }
