c53c9f5b10473e77f3125730088a33b58916b48f
Vincent Matheo <vincent_matheo246052@yahoo.com>
2021-06-03 13:06:23 +0800

    Logbook Service on Procedure

--- app/src/main/java/com/release/barangayapp/service/LogBookService.java
+++ app/src/main/java/com/release/barangayapp/service/LogBookService.java
@@ -1,11 +1,14 @@
 package com.release.barangayapp.service;
 
 
+import androidx.annotation.NonNull;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.release.barangayapp.model.Announcement;
 import com.release.barangayapp.model.LogBook;
@@ -18,6 +21,7 @@ public class LogBookService {
 
     private DatabaseReference logbookRef;
     private FirebaseDatabase firebaseDatabase;
+    private String key;
 
     public LogBookService(){
         firebaseDatabase = FirebaseDatabase.getInstance();
@@ -27,6 +31,7 @@ public class LogBookService {
 
     //function for getting the data from logbook tree
     public ArrayList<LogBook> getData(){
+
 
         ArrayList<LogBook> logBookList = new ArrayList<>();
 
@@ -52,8 +57,33 @@ public class LogBookService {
 
 
     //function for saving data to the logbook tree
-    public void saveData(LogBook logbookData){
-        logbookRef.push().setValue(logbookData);
+    public void saveData(LogBook logbookData, String userID){
+        Query query = logbookRef.orderByChild("userID").equalTo(userID);
+
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists())
+                {
+                    if ( dataSnapshot.getKey().isEmpty()) {
+                        logbookRef.push().setValue(logbookData);
+                    }
+                    else
+                    {
+                        logbookRef.child(dataSnapshot.getKey()).setValue(logbookData);
+                    }
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 
     //function for deleting data to the logbook tree
