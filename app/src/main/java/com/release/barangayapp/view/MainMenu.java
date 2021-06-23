b3bbd27d2555492c5f3fab4a391f643d77b78e10
channnnssss <chanlcoffin@gmail.com>
2021-06-23 20:54:58 +0800

    Terms and Condition

--- app/src/main/java/com/release/barangayapp/view/MainMenu.java
+++ app/src/main/java/com/release/barangayapp/view/MainMenu.java
@@ -6,18 +6,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import  android.content.Intent;
 import android.os.Bundle;
 
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.release.barangayapp.R;
 
 
 public class MainMenu extends AppCompatActivity {
     private Button Button_user, Button_admin;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        textView= findViewById(R.id.terms);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Terms_and_condition.class));
+            }
+        });
 
         Button_user= findViewById(R.id.User_button);
         Button_user.setOnClickListener(v -> OpenUser());
